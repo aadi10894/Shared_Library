64b62d2949cbc056d62e2cd517fade6ad403a8d5
aadi10894 <55064617+aadi10894@users.noreply.github.com>
2021-08-06 15:27:44 -0400

    Update uatInput.groovy

--- src/com/aadi/uatInput.groovy
+++ src/com/aadi/uatInput.groovy
@@ -3,7 +3,7 @@ package com.aadi
 public class uatInput {
 
   def buildIsUatApproved() {
-    def file = new File("C:\_tools\approved.txt")
+    def file = new File("C:/_tools/approved.txt")
     
     if (file.exists()){
       return true;
